99fc407b9dd2ff6b8c566ced8cd413a3b791b647
adyopo <aoporanu@gmail.com>
2019-11-16 10:53:06 +0200

    create stock on location

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Movement;
 use App\Stock;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
+use phpDocumentor\Reflection\Location;
 
 class StocksController extends Controller
 {
@@ -111,5 +113,26 @@ class StocksController extends Controller
         $movement->moved_to = 'moved to ' . $stock->name;
         $movement->save();
         return $movement;
+    }
+
+    /**
+     * @param Request $request
+     * @return ResponseFactory|Response
+     */
+    public function addStockOnLocation(Request $request)
+    {
+        $stock = Stock::findOrFail($request->get('stock_id'));
+        if (!$stock) {
+            return response()->json(['error' => 'Stock not found']);
+        }
+        if ($stock->qty <= 0) {
+            return response()->json(['error' => 'QTY was zero for the selected stock. Please choose another']);
+        }
+        $qty = $request->get('qty');
+
+        $location = Location::find($request->get('location_id'));
+        $location->createStockOnLocation($location, $stock, $qty);
+
+        return response(['stock' => $stock, 'location' => $location, 'qty' => $qty], 201);
     }
 }
--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    /**
+     * @param $location
+     * @param $stock
+     * @param $qty
+     */
+    public function createStockOnLocation($location, $stock, $qty)
+    {
+
+    }
 }
