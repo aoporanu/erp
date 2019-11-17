21a3877975b5fd7aa63e00304f4aebcb82746763
adyopo <aoporanu@gmail.com>
2019-11-17 23:59:26 +0200

    update stock on location if stock already exists on location

--- app/Location.php
+++ app/Location.php
@@ -8,7 +8,10 @@ class Location extends Model
 {
     protected $fillable = [
         'name',
-        'description'
+        'description',
+        'parent',
+        'product_id',
+        'qty'
     ];
 
     /**
@@ -16,9 +19,27 @@ class Location extends Model
      * @param $stock
      * @param $qty
      * @return bool
+     * @throws \Throwable
      */
     public function createStockOnLocation($location, $stock, $qty)
     {
-        return self::save(['location_name' => $location->name, 'stock_id' => $stock->id, 'qty' => $qty]);
+        $stockOnLocation = self::where([
+            'name' => $location->name,
+            'parent' => $stock->id,
+            'product_id' => 1,
+            'description' => $location->name . ' for ' . $stock->id
+        ])->first();
+
+        if ($stockOnLocation) {
+            return $stockOnLocation->update(['qty' => $stockOnLocation->qty + $qty]);
+        }
+
+        return self::create([
+            'name' => $location->name,
+            'parent' => $stock->id,
+            'qty' => $qty,
+            'product_id' => 1,
+            'description' => $location->name . ' for ' . $stock->id,
+        ]);
     }
 }
